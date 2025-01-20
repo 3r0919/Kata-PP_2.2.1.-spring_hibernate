37a3507c8373554ffc62e4735b7b62b6b946c2c0
Dmitry Eronin <dimaromanov06@icloud.com>
2025-01-20 22:23:16 +0300

    Complete task 2.2.1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,40 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car toyota = new Car("Toyota", 2);
+      Car lexus = new Car("Lexus", 300);
+      Car chery = new Car("Chery", 4);
+      Car chevrolet = new Car("Chevrolet", 50);
+
+      userService.add(user1.setCar(toyota).setUser(user1));
+      userService.add(user2.setCar(lexus).setUser(user2));
+      userService.add(user3.setCar(chery).setUser(user3));
+      userService.add(user4.setCar(chevrolet).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user.toString() + " - " + user.getCar().toString());
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,41 @@
+package hiber.dao;
+
+import hiber.model.User;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCar(String model, int series) {
+        String query = "from User user where user.car.model = :model and user.car.series = :series";
+        TypedQuery<User> typedQuery = sessionFactory.getCurrentSession().createQuery(query);
+        typedQuery.setParameter("model", model).setParameter("series", series);
+        return typedQuery.setMaxResults(1).getSingleResult();
+    }
+
+
+}
